93354090b2f14a4e87011711db48ecf1b9cb8fb1
Erik Awwad <eawwad@censys.com>
2025-02-05 10:42:23 -0800

    fix: add memory limit option

--- gobmp_test.go
+++ gobmp_test.go
@@ -4,14 +4,18 @@
 
 package gobmp
 
-import "testing"
-import "image"
-import "image/color"
-import "image/png"
-import "os"
-import "io/ioutil"
-import "bytes"
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"image"
+	"image/color"
+	"image/png"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+)
 
 func readImageFromFile(t *testing.T, srcFilename string) image.Image {
 	var err error
@@ -89,7 +93,7 @@ func compareFiles(t *testing.T, expectedFN string, actualFN string) {
 
 	if len(expectedBytes) != len(actualBytes) {
 		t.Logf("%s and %s differ in size %d %d \n",
-		 expectedFN, actualFN, len(expectedBytes), len(actualBytes))
+			expectedFN, actualFN, len(expectedBytes), len(actualBytes))
 		t.Fail()
 		return
 	}
@@ -229,5 +233,17 @@ func TestDecode(t *testing.T) {
 		writeImageToFile(t, m, dstFN, "png", nil)
 		// this fails, though they look about the same
 		// compareFiles(t, expectedFN, dstFN)
+	}
+}
+
+func TestMemoryLimit(t *testing.T) {
+	assert := assert.New(t)
+	files, _ := filepath.Glob("testdata/memorylimit/*.bmpb")
+	for _, f := range files {
+		icoData, err := os.ReadFile(f)
+		assert.NoError(err, f)
+		r := bytes.NewReader(icoData)
+		_, err = Decode(r, WithMemoryLimit(10_000_000))
+		assert.ErrorIs(err, ErrMemoryLimitExceeded)
 	}
 }
--- readme.md
+++ readme.md
@@ -20,6 +20,34 @@ cause the image.Decode function to support reading BMP files.
 The documentation may be read online at
 [GoDoc](http://godoc.org/github.com/censys-oss/gobmp)
 
+Usage
+-----
+
+```go
+reader, err := os.Open("example.bmp")
+if err != nil {
+        log.Fatal(err)
+}
+defer reader.Close()
+
+// To decode and return image
+image, err := Decode(r)
+if err != nil {
+        log.Fatal(err)
+}
+
+// To decode, with memory limits
+image, err := Decode(r, WithMemoryLimit(10_000_000))
+if err != nil {
+        log.Fatal(err)
+}
+```
+
+
+Dependencies
+------------
+
+Does not import external dependencies
 
 Status
 ------
@@ -30,11 +58,15 @@ By default, the encoder will write a 24-bit RGB image, or a 1-, 4-, or 8-bit
 paletted image. Support for 32-bit RGBA images can optionally be enabled.
 Writing compressed images is not supported.
 
+This library is vetted to handle decoding without panicking and can return an error if allocation hits a defined limit.
+
+See `todo.md` for what could be worked on.
+
 
 License
 -------
 
-Gobmp is distributed under an MIT-style license. Refer to the COPYING.txt
+Gobmp is distributed under an MIT-style license. Refer to the LICENSE.txt
 file.
 
 Copyright &copy; 2025 Censys
